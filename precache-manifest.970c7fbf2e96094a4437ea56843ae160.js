848205bd0f9d814df08e3784ad73c12f5fe66596
Timotei Dumitru <dumitru.timy@gmail.com>
2019-08-10 18:40:07 +0100

    Updates

---
+++ precache-manifest.970c7fbf2e96094a4437ea56843ae160.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60084050dcddcee9214ad024361e4c75",
+    "url": "/phase-eight/index.html"
+  },
+  {
+    "revision": "11a7b1ec896542114c10",
+    "url": "/phase-eight/static/css/2.d4c5f937.chunk.css"
+  },
+  {
+    "revision": "fc4be56632429626ff6f",
+    "url": "/phase-eight/static/css/main.4a27deca.chunk.css"
+  },
+  {
+    "revision": "11a7b1ec896542114c10",
+    "url": "/phase-eight/static/js/2.57690721.chunk.js"
+  },
+  {
+    "revision": "fc4be56632429626ff6f",
+    "url": "/phase-eight/static/js/main.b814bfd0.chunk.js"
+  },
+  {
+    "revision": "342f67bb07da2c4608f7",
+    "url": "/phase-eight/static/js/runtime~main.0cd42760.js"
+  }
+]);
