f69f97ab843eedcb40c6a00a24b202a7393bd601
Timotei Dumitru <dumitru.timy@gmail.com>
2019-08-11 11:01:28 +0100

    build data links for footer section

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,6 +4,44 @@ import { makeStyles } from '@material-ui/core/styles'
 
 export default function Footer() {
 
+  const content = [
+    hereToHelp = {
+      link_header: 'Here to Help',
+      link_zero: 'Contact Us',
+      link_one: 'Delivery & Collection',
+      link_two: 'Returns',
+      link_three: 'FAQs',
+    },
+    shopping = {
+      link_header: 'Shopping',
+      link_zero: 'Store Locator',
+      link_one: 'Track Your Order',
+      link_two: 'Size Guides',
+      link_three: 'Gift Cards',
+      link_four: 'Gift Packaging',
+      link_five: 'Personal Styling',
+      link_six: 'Refer a Friend',
+    },
+    aboutUs = {
+      link_header: 'About Us',
+      link_zero: 'Our Story',
+      link_one: 'Corporate Responsibility',
+      link_two: 'Careers',
+      link_three: 'Affiliates',
+      link_four: 'Reviews',
+    },
+    legal = {
+      link_header: 'Legal',
+      link_zero: 'Terms & Conditions',
+      link_one: 'Competitions & Offers',
+      link_two: 'Privacy Policy',
+      link_three: 'Modern Slavery Statement',
+      link_four: 'Cookies',
+      link_five: 'Fraudulent Websites',
+      link_six: 'Accessibility',
+    },
+  ]
+
   const useStyles = makeStyles(theme => ({
     footerMain: {
       backgroundColor: '#f3f3f3',
